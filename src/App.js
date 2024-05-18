ee9b7fb7dcd79efc67f5c47d240ef0d5ddcbc740
Eric Obermühlner <eobermuhlner@gmail.com>
2024-05-18 12:34:11 +0200

    use bootstrap main navigation

--- src/App.js
+++ src/App.js
@@ -7,17 +7,35 @@ import DataImport from './pages/DataImport';
 function App() {
   return (
     <Router>
-      <div className="navigation">
-        <Link to="/browse">Browse</Link>
-        <Link to="/search">Search</Link>
-        <Link to="/import">Import</Link>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">Ammonite App</Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav me-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to="/browse">Browse</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/search">Search</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/import">Import</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div className="container mt-3">
+        <Routes>
+          <Route path="/browse" element={<AmmoniteBrowse />} />
+          <Route path="/search" element={<AmmoniteSearch />} />
+          <Route path="/import" element={<DataImport />} />
+          <Route path="/ammonite/:id" element={<AmmoniteViewer />} />
+        </Routes>
       </div>
-      <Routes>
-        <Route path="/browse" element={<AmmoniteBrowse />} />
-        <Route path="/search" element={<AmmoniteSearch />} />
-        <Route path="/import" element={<DataImport />} />
-        <Route path="/ammonite/:id" element={<AmmoniteViewer />} />
-      </Routes>
     </Router>
   );
 }
