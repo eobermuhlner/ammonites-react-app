1f09ca0aa1c097ca498e497fe55ad9f66200a0df
Eric Obermühlner <eric.obermuehlner@assetmax.ch>
2024-05-19 14:14:32 +0200

    support login/logout

--- src/App.js
+++ src/App.js
@@ -6,31 +6,60 @@ import AmmoniteViewer from './pages/AmmoniteViewer';
 import DataImport from './pages/DataImport';
 import Login from './pages/Login';
 import Navbar from './components/Navbar';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      setIsAuthenticated(true);
-    }
-  }, []);
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            setIsAuthenticated(true);
+        }
+    }, []);
 
-  return (
-      <Router>
-        <Navbar isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />
-        <div className="container mt-3">
-          <Routes>
-            <Route path="/browse" element={<AmmoniteBrowse />} />
-            <Route path="/search" element={<AmmoniteSearch />} />
-            <Route path="/import" element={<DataImport />} />
-            <Route path="/ammonite/:id" element={<AmmoniteViewer />} />
-            <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
-          </Routes>
-        </div>
-      </Router>
-  );
+    return (
+        <Router>
+            <Navbar isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />
+            <div className="container mt-3">
+                <Routes>
+                    <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
+                    <Route
+                        path="/browse"
+                        element={
+                            <PrivateRoute isAuthenticated={isAuthenticated}>
+                                <AmmoniteBrowse />
+                            </PrivateRoute>
+                        }
+                    />
+                    <Route
+                        path="/search"
+                        element={
+                            <PrivateRoute isAuthenticated={isAuthenticated}>
+                                <AmmoniteSearch />
+                            </PrivateRoute>
+                        }
+                    />
+                    <Route
+                        path="/import"
+                        element={
+                            <PrivateRoute isAuthenticated={isAuthenticated}>
+                                <DataImport />
+                            </PrivateRoute>
+                        }
+                    />
+                    <Route
+                        path="/ammonite/:id"
+                        element={
+                            <PrivateRoute isAuthenticated={isAuthenticated}>
+                                <AmmoniteViewer />
+                            </PrivateRoute>
+                        }
+                    />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
